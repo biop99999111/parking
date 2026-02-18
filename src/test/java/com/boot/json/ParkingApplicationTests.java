8983d177b763d2c851786e0f0bd24a29251c48c1
admin <admin@175.213.116.12>
2026-02-18 21:05:45 +0900

    Initial commit

---
+++ src/test/java/com/boot/json/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
