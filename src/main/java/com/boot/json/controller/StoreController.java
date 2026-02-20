b974fe01b2ff1321005c6c8a555079db033a0079
1234 <1234@HiClass15>
2026-02-20 13:47:35 +0900

    store서비스

--- src/main/java/com/boot/json/controller/StoreController.java
+++ src/main/java/com/boot/json/controller/StoreController.java
@@ -1,5 +1,17 @@
-package com.boot.json.controller;
-
+@RestController
+@RequestMapping("/store")
+@RequiredArgsConstructor
 public class StoreController {
 
+    private final StoreService storeService;
+
+    @PostMapping("/discount")
+    public ResponseEntity<String> applyDiscount(
+            @RequestParam Long storeId,
+            @RequestParam String carNo,
+            @RequestParam int discountMinutes) {
+
+        storeService.applyStoreDiscount(storeId, carNo, discountMinutes);
+        return ResponseEntity.ok("할인 적용 완료");
+    }
 }
