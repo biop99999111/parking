31213961c7b3dec9f2f12c9cc0f302714c480cd1
1234 <1234@HiClass15>
2026-02-20 13:55:32 +0900

    storeCotroller

--- src/main/java/com/boot/json/controller/StoreController.java
+++ src/main/java/com/boot/json/controller/StoreController.java
@@ -1,3 +1,15 @@
+package com.boot.json.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.json.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/store")
 @RequiredArgsConstructor
