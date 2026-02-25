8e96eb06d43c4945009cfde1febc4185be245628
1234 <1234@HiClass15>
2026-02-25 16:00:06 +0900

    수정

--- src/main/java/com/boot/json/model/StoreMapper.java
+++ src/main/java/com/boot/json/model/StoreMapper.java
@@ -4,23 +4,25 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Map;
 
-
-
-
 @Mapper
 public interface StoreMapper {
 
+	     // 차량 검색
 	    List<Map<String, Object>> searchCar(@Param("carNo") String carNo);
 
-	     //매장 잔여 쿠폰 조회
-	    int getCouponCount(@Param("storeId") Long storeId);
+	     // 매장 잔여 쿠폰 조회
+	    int getCouponCount(@Param("storeName") String storeName);
 	    
-         // 쿠폰 수량 차감
-	    int updateStoreCouponDecrease(@Param("storeId") Long storeId);
+         // 쿠폰 할인 금액 
+	    int getCouponAmount(@Param("storeName") String storeName);
+	    
+	    // 매장쿠폰 1장 감소
+	    int decreaseCoupon(@Param("storeName") String storeName);
 	    
 	     // 차량에 쿠폰 적용
-	    int applyCouponToCar(@Param("carNo") String carNo);  
-	}
+	    int applyCouponToCar(@Param("carNo") String carNo,
+                @Param("couponAmount") int couponAmount);
+}
 
 
 
