4d260644a7b770116c214e2c44299c369a176be7
jhpark0623 <as2248@naver.com>
2026-02-25 14:39:03 +0900

    ....

--- src/main/java/com/boot/json/service/AdminService.java
+++ src/main/java/com/boot/json/service/AdminService.java
@@ -13,28 +13,4 @@ public class AdminService {
 
 	@Autowired
     private AdminMapper adminMapper;
-
-	// 🚗 차량 검색 기능
-    public List<Car> searchCars(String keyword) {
-        if (keyword == null || keyword.isEmpty()) {
-            return adminMapper.getAllCars();
-        } else {
-            return adminMapper.searchCarsByKeyword(keyword);
-        }
-    }
-
-//    // 페이징 처리된 주차 내역 조회
-//    public List<Car> getHistoryList(Criteria cri) {
-//        return adminMapper.getListWithPaging(cri);
-//    }
-//
-//    // 전체 데이터 개수 (PageDTO 생성용)
-//    public int getTotalCount(Criteria cri) {
-//        return adminMapper.getTotalCount(cri);
-//    }
-//
-//    // 오늘 총 매출 합계 요약
-//    public int getTodayTotalRevenue() {
-//        return adminMapper.selectTodayRevenue();
-//    }
 }
