f95050abd30e977ee9227aecfdcef171de0ce48e
jhpark0623 <as2248@naver.com>
2026-02-25 14:40:22 +0900

    ..

--- src/main/java/com/boot/json/controller/AdminController.java
+++ src/main/java/com/boot/json/controller/AdminController.java
@@ -25,15 +25,5 @@ public class AdminController {
     public String loginPage() {
         return "login"; // login.html
     }
-
-    // 관리자 차량 목록 조회(사용목록)
-    @GetMapping("/admin/cars")
-    public String getCarList(@RequestParam(defaultValue = "") String keyword, Model model) {
-        List<Car> carList = adminService.searchCars(keyword); // 검색어 없으면 전체
-
-        model.addAttribute("carList", carList);
-        model.addAttribute("keyword", keyword);
-
-        return "carList"; // 차량 목록 페이지..
-    }
+    
 }
